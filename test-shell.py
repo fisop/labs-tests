9986af6952d3578f42bfdd1d2555b3a1da38c0bd
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2021-09-04 23:57:54 -0300

    Refactor and colors
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- test-shell.py
+++ test-shell.py
@@ -1,13 +1,17 @@
 #! /usr/bin/python3 
 import os
 import shutil
+import sys
 import tempfile
 from subprocess import Popen, PIPE, STDOUT
+from termcolor import cprint
+
+def multiline(cmds):
+    return '\n'.join([cmd.strip() for cmd in cmds]) + '\n'
 
 def compare_strings(current, expected):
     if current != expected:
-        print(
-        """
+        message="""
         Expected:
 
         {}
@@ -15,9 +19,9 @@ def compare_strings(current, expected):
         But got:
         
         {}
-        """.format(expected.encode() if expected else None, current.encode() if current else None))
-        return False
-    return True
+        """.format(expected.encode() if expected else None, current.encode() if current else None)
+        return message
+    return None
 
 def launch_test(name, stdin=b'echo hi', shell_binary='./example-shell/sh', timeout=100, expected_stdout=None, expected_stderr=None, check_stdout=False, check_stderr=False, shell=True):
     """launch_test execute an instance of the shell binary to test (located at _shell_binary_)
@@ -25,55 +29,46 @@ def launch_test(name, stdin=b'echo hi', shell_binary='./example-shell/sh', timeo
     If both outputs match, then it returns True indicating a successful test. Otherwise it returns false.
     A _timeout_ can be set to limit the excecution.
     """
-    print("\nRunning test: {}".format(name))
-    try:
-        p = Popen(shell_binary, stdin=PIPE, stdout=PIPE, stderr=STDOUT, shell=shell)
-        (stdout, stderr) = p.communicate(input=stdin, timeout=timeout)
-        if check_stdout:
-            if not compare_strings(stdout.decode() if stdout else None, expected_stdout):
-                raise Exception("Check stdout failed")
-        if check_stderr:
-            if not compare_strings(stderr.decode() if stderr else None, expected_stderr):
-                raise Exception("Check stderr failed")
-    except Exception as e:
-        print("""FAIL test: {}""".format(name))
-        print("Excception ocurred: {}".format(e))
-        return False
-    print("PASS test: {}".format(name))
-    return True
+    p = Popen(shell_binary, stdin=PIPE, stdout=PIPE, stderr=STDOUT, shell=shell)
+    (stdout, stderr) = p.communicate(input=stdin, timeout=timeout)
+    if check_stdout:
+        msg = compare_strings(stdout.decode() if stdout else None, expected_stdout)
+        if msg:
+            raise Exception("Check stdout failed: {}".format(msg))
+    if check_stderr:
+        msg = compare_strings(stderr.decode() if stderr else None, expected_stderr)
+        if msg:
+            raise Exception("Check stderr failed: {}".format(msg))
 
-shell_binary="./example-shell/sh"
-reflector_aux="/home/vagrant/lab-tests/reflector"
-
-def test_simple_echo():
-    global shell_binary
+def test_simple_echo(shell_binary: str):
     name="Test simple echo command"
-    command="echo hi\n"
+    stdin="echo hi\n"
     expected_stdout="hi\n"
     launch_test(name,
-        stdin=command.encode(),
+        stdin=stdin.encode(),
         shell_binary=shell_binary,
         expected_stdout=expected_stdout,
         check_stdout=True
         )
 
-def test_simple_env():
-    global shell_binary
+def test_simple_env(shell_binary: str):
     name="Test simple env "
-    command="env\nHELLO=4 env\nenv"
+    stdin=multiline("""env
+                    HELLO=4 env
+                    env""".splitlines())
     expected_stdout="HELLO=4\n"
+    command="sh -c \"{}\"".format(shell_binary + " | grep HELLO")
     launch_test(name,
-        stdin=command.encode(),
-        shell_binary="sh -c \"" + shell_binary + " | grep HELLO\"",
+        stdin=stdin.encode(),
+        shell_binary=command,
         expected_stdout=expected_stdout,
         expected_stderr=None,
         check_stdout=True,
         check_stderr=True
         )
 
-def test_redirect_does_not_create():
+def test_redirect_does_not_create(shell_binary: str):
     # sleep 100 <in.txt >out.txt
-    global shell_binary
     global tempdir
     filename=os.path.join(tempdir, "no-existe.txt")
     name="Test stdin redirect does not create file"
@@ -88,8 +83,7 @@ def test_redirect_does_not_create():
         check_stderr=True
         )
 
-def test_redirect_does_not_leak_fds():
-    global shell_binary
+def test_redirect_does_not_leak_fds(shell_binary: str):
     global tempdir
     global reflector_aux
     filename=os.path.join(tempdir, "leaks-redirect.txt")
@@ -105,47 +99,96 @@ def test_redirect_does_not_leak_fds():
         check_stderr=True
         )
 
-def test_pipes_does_not_leak_fds():
-    global shell_binary
+def test_pipes_does_not_leak_fds(shell_binary: str):
     global tempdir
     global reflector_aux
     filename=os.path.join(tempdir, "leaks-pipes.txt")
     name="Test pipes does leak fds"
-    command="echo hi | cat - | cat - | {} {}\n".format(reflector_aux, filename)
-    expected_stdout="3 {}\n".format(filename)
+    stdin=multiline("echo hi | cat - | cat - | {} {}".format(reflector_aux, filename).splitlines())
+    expected_stdout=multiline("3 {}".format(filename).splitlines())
+    command="HOME=/home sh -c \"{}\"".format(
+            shell_binary + " 2>/dev/null 1>/dev/null; wc -l {}".format(filename))
     launch_test(name,
-        stdin=command.encode(),
-        shell_binary="sh -c \"" + shell_binary + " 2>/dev/null 1>/dev/null; wc -l {}\"".format(filename),
+        stdin=stdin.encode(),
+        shell_binary=command,
         expected_stdout=expected_stdout,
         expected_stderr=None,
         check_stdout=True,
         check_stderr=True
         )
 
-def test_cd():
-    global shell_binary
+def test_cd(shell_binary: str):
     global reflector_aux
     filename=os.path.join(tempdir, "leaks-pipes.txt")
     name="Test pipes does leak fds"
-    command="cd /home\n/bin/pwd\ncd /proc\n/bin/pwd\ncd sys\n/bin/pwd\ncd kernel\n/bin/pwd\ncd ..\n/bin/pwd\ncd\n/bin/pwd\n".format(reflector_aux, filename)
-    expected_stdout="/home\n/proc\n/proc/sys\n/proc/sys/kernel\n/proc/sys\n/home\n"
+    stdin=multiline(["cd /home",
+            "/bin/pwd",
+            "cd /proc",
+            "/bin/pwd",
+            "cd sys",
+            "/bin/pwd",
+            "cd kernel",
+            "/bin/pwd",
+            "cd ..",
+            "/bin/pwd",
+            "cd",
+            "/bin/pwd"])
+    expected_stdout=multiline("""/home
+                              /proc
+                              /proc/sys
+                              /proc/sys/kernel
+                              /proc/sys
+                              /home""".splitlines())
+
+    command="HOME=/home sh -c \"{}\"".format(
+            shell_binary)
+
     launch_test(name,
-        stdin=command.encode(),
-        shell_binary="HOME=/home sh -c \"" + shell_binary + "\"",
+        stdin=stdin.encode(),
+        shell_binary=command,
         expected_stdout=expected_stdout,
         expected_stderr=None,
         check_stdout=True,
         check_stderr=True
         )
 
-tempdir=tempfile.mkdtemp(suffix='-shell-test')
-print("Test results will be stored in {}".format(tempdir))
 
-test_simple_echo()
-test_simple_env()
-test_redirect_does_not_create()
-test_redirect_does_not_leak_fds()
-test_pipes_does_not_leak_fds()
-test_cd()
+def run_tests(shell_binary):
+    global tempdir
+    global reflector_aux
+    tests = [
+            test_simple_echo,
+            test_simple_env,
+            test_redirect_does_not_create,
+            test_redirect_does_not_leak_fds,
+            test_pipes_does_not_leak_fds,
+            test_cd
+            ]
+    reflector_aux="/home/vagrant/lab-tests/reflector"
+    tempdir=tempfile.mkdtemp(suffix='-shell-test')
+    print("Test results will be stored in {}".format(tempdir))
 
-shutil.rmtree(tempdir)
+    count = 1
+    failed = 0
+    total = len(tests)
+    for test in tests:
+        try:
+            test(shell_binary)
+            cprint("PASS {}/{}: {}".format(count, total, test.__name__), "green")
+        except Exception as e:
+            cprint("FAIL {}/{}: {}. Exception ocurred: {}".format(count, total, test.__name__, e), "red")
+            failed += 1
+        finally:
+            count += 1
+
+    cprint("{} out of {} tests passed".format(total - failed, total), "yellow" if failed else "green")
+
+    shutil.rmtree(tempdir)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("{}: {} <test-shell-binary>".format(sys.argv[0], sys.argv[0]))
+        exit()
+    shell_binary=sys.argv[1]
+    print(shell_binary)
+    run_tests(shell_binary)
