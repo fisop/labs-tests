65cb7d7d168d78926a7ea536a8dd9c52157e4ea2
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2021-09-05 22:45:56 -0300

    feat: add utils lib

---
+++ fork/utils.py
@@ -0,0 +1,12 @@
+COLORS = {
+    'default': "\033[0m",
+    'red': "\033[31m",
+    'green': "\033[32m",
+    'yellow': "\033[33m"
+}
+
+def color(text, color_name):
+    return COLORS[color_name] + text + COLORS["default"]
+
+def format_result(result):
+    return color('OK', 'green') if result else color('FAIL', 'red')
