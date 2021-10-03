6b2129f986bdcf2a85d228e8ad35a85215020ae7
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2021-10-03 20:09:58 -0300

    Pruebas automáticas para el lab shell [WIP] (#1)
    * Inital commit
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Inital commit
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Added more tests
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Added more tests
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Refactor and colors
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Read tests from file
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Read tests from directory
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Some more refactoring
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Added shell-no-interactive.patch
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * More tests and dont encode diff
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Moved everything to shell subdirectory
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Added redirect tests
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Test that stdout uses O_TRUNC
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Fix test so that bash passes
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * More tests and small fixes
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>
    
    * Exposed reflector program and pimped up Makefile
    
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

---
+++ shell/reflector.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int inspect_fds(int out_fd) {
+    char procdir[100];
+
+    int res = sprintf(procdir, "/proc/%d/fd", getpid());
+
+    DIR* dir = opendir(procdir);
+    int dir_fd = dirfd(dir);
+
+    struct dirent* entry;
+    while ((entry = readdir(dir))) {
+        if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, "..")) {
+            continue;
+        }
+
+        char target_link_buf[100];
+        int len = readlinkat(dir_fd, entry->d_name, target_link_buf, 100);
+        target_link_buf[len] = 0;
+
+        if (dir_fd == atoi(entry->d_name) || out_fd == atoi(entry->d_name)) {
+            continue;
+        }
+        // if (!strcmp(target_link_buf, procdir)) {
+        //    continue;
+        //}
+
+        dprintf(out_fd, "%s\t%s\n", entry->d_name, target_link_buf);
+    }
+    close(dir_fd);
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: reflector <target_file>\n");
+    }
+    
+    int out_fd = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+    if (out_fd < 0) {
+        perror("Error en open");
+        return -1;
+    }
+    inspect_fds(out_fd);
+
+    close(out_fd);
+    return 0;
+}
